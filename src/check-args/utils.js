e7df9d881a23046929328ffe71d255a01c01f491
SidneyNemzer <sidney@ucode.com>
2018-08-21 23:06:44 -0700

    Progress

---
+++ src/check-args/utils.js
@@ -0,0 +1,14 @@
+export const isString = value =>
+  typeof value === 'string'
+
+export const isArray = value =>
+  value instanceof Array
+
+export const isObject = value =>
+  typeof value === 'object' && value !== null
+
+export const isNumber = value =>
+  typeof value === 'number'
+
+export const isBoolean = value =>
+  typeof value === 'boolean'
